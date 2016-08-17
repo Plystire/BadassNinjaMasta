d13942276b53ad9acabd11cfa38ba1ebd77c10cd
Plystire <plystiregryatto@hotmail.com>
2016-08-16 20:46:15 -0600

    Initial Networking commit

---
+++ Assets/Scripts/AssassinsBladeController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssassinsBladeController : MonoBehaviour {
+
+    private Transform sword;
+    private Transform scabbard;
+
+	// Use this for initialization
+	void Start () {
+        sword = transform.FindChild("Sword");
+        scabbard = transform.FindChild("Scabbard");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/RoomBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Photon;
+
+public class RoomBehavior : Photon.PunBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.autoJoinLobby = true;
+        PhotonNetwork.ConnectUsingSettings("0.0.1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+    new public void OnConnectedToMaster()
+    {
+        if (!PhotonNetwork.autoJoinLobby)
+            PhotonNetwork.autoJoinLobby = true;
+    }
+
+    new public void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+}
